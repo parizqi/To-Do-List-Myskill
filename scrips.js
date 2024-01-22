7cb05cbef2c370a49c1463e90068cbac61583c21
parizqi <parizqiaminullah@gmail.com>
2024-01-22 14:34:46 -0800

    done

--- scrips.js
+++ scrips.js
@@ -1,50 +1,50 @@
-var addbutton = document.getElementById("btnadd");
-addbutton.addEventListener("click", addItem);
-function addItem() {
-  alert("Add Button Clicked");
+var addButton = document.getElementById("btnadd");
+addButton.addEventListener("click", addToDoItem);
+function addToDoItem() {
+    alert("Add button clicked!");
 }
 
-var textItem = document.getElementById("texisi");
-var isiList = document.getElementById("hasil-list");
+var toDoEntryBox = document.getElementById("textisi");
+var toDoList = document.getElementById("hasil-list");
 
 function newToDoItem(itemText, completed) {
-  var toDoItem = document.createElement("li");
-  var toDoText = document.createTextNode(itemText);
-  toDoItem.appendChild(toDoText);
+    var toDoItem = document.createElement("li");
+    var toDoText = document.createTextNode(itemText);
+    toDoItem.appendChild(toDoText);
 
-  if (completed) {
-    toDoItem.classList.add("completed");
-  }
+    if (completed) {
+        toDoItem.classList.add("completed");
+    }
 
-  isiList.appendChild(toDoItem);
-  toDoItem.addEventListener("dblclick", toggleToDoItemState);
+    toDoList.appendChild(toDoItem);
+    toDoItem.addEventListener("dblclick", toggleToDoItemState);
 }
 
-function addItem() {
-  var itemText = toDoEntryBox.value;
-  newToDoItem(itemText, false);
+function addToDoItem() {
+    var itemText = toDoEntryBox.value;
+    newToDoItem(itemText, false);
 }
 
 function toggleToDoItemState() {
   if (this.classList.contains("completed")) {
-    this.classList.remove("completed");
+      this.classList.remove("completed");
   } else {
-    this.classList.add("completed");
+      this.classList.add("completed");
   }
 }
 
-function btncomplate() {
+function clearCompleted() {
   var completedItems = toDoList.getElementsByClassName("completed");
 
   while (completedItems.length > 0) {
-    completedItems.item(0).remove();
+      completedItems.item(0).remove();
   }
 }
 
 function emptyList() {
   var toDoItems = toDoList.children;
   while (toDoItems.length > 0) {
-    toDoItems.item(0).remove();
+      toDoItems.item(0).remove();
   }
 }
 
@@ -55,36 +55,37 @@ alert(myArray[0]);
 //This will alert "something to store"
 
 var toDoInfo = {
-  task: "Thing I need to do",
-  completed: false,
+  "task": "Thing I need to do",
+  "completed": false
 };
 
 function saveList() {
   var toDos = [];
 
   for (var i = 0; i < toDoList.children.length; i++) {
-    var toDo = toDoList.children.item(i);
+      var toDo = toDoList.children.item(i);
 
-    var toDoInfo = {
-      task: toDo.innerText,
-      completed: toDo.classList.contains("completed"),
-    };
+      var toDoInfo = {
+          "task": toDo.innerText,
+          "completed": toDo.classList.contains("completed")
+      };
 
-    toDos.push(toDoInfo);
+      toDos.push(toDoInfo);
+
   }
 
   localStorage.setItem("toDos", JSON.stringify(toDos));
-  console.log("masuk kesini ya");
+  console.log("masuk kesini ya")
 }
 
 function loadList() {
   if (localStorage.getItem("toDos") != null) {
-    var toDos = JSON.parse(localStorage.getItem("toDos"));
+      var toDos = JSON.parse(localStorage.getItem("toDos"));
 
-    for (var i = 0; i < toDos.length; i++) {
-      var toDo = toDos[i];
-      newToDoItem(toDo.task, toDo.completed);
-    }
+      for (var i = 0; i < toDos.length; i++) {
+          var toDo = toDos[i];
+          newToDoItem(toDo.task, toDo.completed);
+      }
   }
 }
 
